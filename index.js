a794c660e4159047dc5ff562cfb40b1e206af181
Jurij Repar <jurij.repar@bitstamp.net>
2022-05-11 12:02:34 +0200

    Initial release

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
-var postcss = require('postcss')
-require('colors')
+var postcss = require('postcss');
+require('colors');
 
-module.exports = postcss.plugin('postcss-wrap', function (opts) {
+module.exports = (opts = {}) => {
   opts = opts || {}
 
   var selectorsBlacklist = [
     /^from/,
     /^to/,
     /\%$/
-  ]
+  ];
 
   if (opts.skip instanceof Array) {
-    selectorsBlacklist = selectorsBlacklist.concat(opts.skip)
+    selectorsBlacklist = selectorsBlacklist.concat(opts.skip);
   } else if (opts.skip instanceof RegExp) {
-    selectorsBlacklist.push(opts.skip)
+    selectorsBlacklist.push(opts.skip);
   }
 
-  return function (css, result) {
-    if (!opts.selector) {
-      result.warn('opts.selector must be specified'.red)
-      return
-    }
+  return {
+    postcssPlugin: 'postcss-wrap',
+    OnceExit: function (css, _ref) {
 
-    css.walkRules(function (rule) {
-      rule.selectors = rule.selectors.map(function (selector) {
-        for (var i = 0; i < selectorsBlacklist.length; i++) {
-          if (selector.match(selectorsBlacklist[i])) {
-            return selector
+      if (!opts.selector) {
+        _ref.result.warn('opts.selector must be specified');
+        return;
+      }
+
+      css.walkRules(function(rule) {
+        rule.selectors = rule.selectors.map(function(selector) {
+          for (var i = 0; i < selectorsBlacklist.length; i++) {
+            if (selector.match(selectorsBlacklist[i])) {
+              return selector;
+            }
           }
-        }
-        return opts.selector + ' ' + selector
-      })
-    })
-  }
-})
+          return opts.selector + ' ' + selector;
+        });
+      });
+    }
+  };
+};
+
+module.exports.postcss = true;
